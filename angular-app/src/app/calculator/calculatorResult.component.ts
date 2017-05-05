50511f3c4787eb92399a96246594a40902081288
Magesh <tkmagesh77@gmail.com>
2017-05-05 14:45:26 +0530

    conditional styling

--- angular-app/src/app/calculator/calculatorResult.component.ts
+++ angular-app/src/app/calculator/calculatorResult.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
 	selector : 'calculator-result',
-	template : '<div class="result">{{result}}</div>',
-	styles : ['.result { font-weight : bold; font-size:22pt}']
+	template : '<div class="result" [ngClass]="{positive : result >= 0, negative : result < 0}">{{result}}</div>',
+	styles : ['.result { font-weight : bold; font-size:22pt}', '.positive { color : green}', '.negative {color : red}']
 
 })
 export class CalculatorResultComponent{
