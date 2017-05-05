a0194d9f0f2ec1785f08218e3828e22cbf91f57c
Magesh <tkmagesh77@gmail.com>
2017-05-05 16:50:30 +0530

    Refactoring 'calculator' to service

--- angular-app/src/app/bugTracker/bugTracker.component.ts
+++ angular-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IBug } from './models/IBug';
 
 @Component({
 	selector : 'bug-tracker',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
 	styleUrls : ['bugTracker.style.css']
 })
 export class BugTrackerComponent{
+	newBugName : string = '';
 
+	bugs : Array<IBug> = [];
+	
+	onAddNewClick(){
+		let newBug : IBug = {
+			name : this.newBugName,
+			isClosed : false
+		};
+		this.bugs.push(newBug);
+	}
+
+	onBugNameClick(bug){
+		bug.isClosed = !bug.isClosed;
+	}
+
+	onRemoveClosedClick(){
+		for(let index = this.bugs.length-1; index >= 0; index--){
+			if (this.bugs[index].isClosed)
+				this.bugs.splice(index,1);
+		}
+	}
+
+	getClosedCount(){
+		let result = 0;
+		for(let bug of this.bugs){
+			if (bug.isClosed){
+				++result;
+			}
+		}
+		return result;
+	}
 }
--- angular-app/src/app/calculator/calculator.component.ts
+++ angular-app/src/app/calculator/calculator.component.ts
@@ -1,31 +1,13 @@
 import { Component } from '@angular/core';
+import { Calculator } from './calculator';
 
 @Component({
 	selector: 'calculator',
 	templateUrl: 'calculator.template.html'
 })
 export class CalculatorComponent{
-	number1 : number = 0;
-	number2 : number = 0;
-	result : number = 0;
-	operation : string = '';
+	calc : Calculator = new Calculator();
 
-	onCalculateClick(){
-		switch (this.operation) {
-			case "add":
-				this.result = this.number1 + this.number2;
-				break;
-			case "subtract":
-				this.result = this.number1 - this.number2;
-				break;
-			case "multiply":
-				this.result = this.number1 * this.number2;
-				break;
-			case "divide":
-				this.result = this.number1 / this.number2;
-				break;
-			
-		}
-	}
+	
 
 }
